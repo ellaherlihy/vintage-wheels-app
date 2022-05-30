41d5a3afcff34e055383a5839aa19c55e40fdcd1
chun man poon <chunman906@hotmail.com>
2022-05-30 16:24:51 +0100

    created car model & empty controller

---
+++ db/migrate/20220530150711_create_cars.rb
@@ -0,0 +1,17 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.string :model
+      t.integer :year
+      t.string :brand
+      t.float :price
+      t.text :features
+      t.string :color
+      t.string :location
+      t.text :description
+      t.references :users, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
