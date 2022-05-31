a7a82f0b20bba0515cb0b3c5a9e5f0df5847488f
Ella Herlihy <ellaherlihy@gmail.com>
2022-05-31 12:53:50 +0100

    index page and show page front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Car.destroy_all
+
+10.times do
+  Car.create(brand: "Ford", model: "Thunderbird", year: "1980", user_id: 1)
+end
+
+p "seeding"
