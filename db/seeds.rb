643731557d3d0c8ec51b2445d041b42c344233a9
chun man poon <chunman906@hotmail.com>
2022-05-31 14:46:15 +0100

    seed - car, user, booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+require 'faker'
+Booking.destroy_all
 Car.destroy_all
+User.destroy_all
+puts 'Creating cars..'
+puts 'Creating users..'
+User.create(email: 'deborah@email.com', password: 'password', first_name: 'Deborah', last_name: 'Reis')
+User.create(email: 'chayan@email.com', password: 'password', first_name: 'Chayan', last_name: 'Agarwal')
+User.create(email: 'chun@email.com', password: 'password', first_name: 'Chun', last_name: 'Man')
+User.create(email: 'ella@email.com', password: 'password', first_name: 'Ella', last_name: 'Herlihy')
 
-10.times do
-  Car.create(brand: "Ford", model: "Thunderbird", year: "1980", user_id: 1)
+def car_prepare
+  model = Faker::Vehicle.manufacture
+  year = Faker::Vehicle.year
+  brand = Faker::Vehicle.make
+  location = Faker::Address.city
+  description = Faker::Vehicle.transmission
+  price = Faker::Number.between(from: 50000, to: 200000)
+  features = Faker::Vehicle.standard_specs
+  color = Faker::Vehicle.color
+  user = User.all.sample.id
+  {user_id: user, model: model, year: year, brand: brand, price: price, features: features, color: color, location: location, description: description}
 end
 
-p "seeding"
+20.times do
+  fake_users = Car.create(car_prepare)
+end
+
+puts 'Creating bookings..'
+  Booking.create( start_date: '05/01/2022' , end_date: '06/01/2022', user_id: User.all.sample.id , car_id: Car.all.sample.id  )
+  Booking.create( start_date: '04/01/2022' , end_date: '07/01/2022', user_id: User.all.sample.id , car_id: Car.all.sample.id )
+  Booking.create( start_date: '03/01/2022' , end_date: '08/01/2022', user_id: User.all.sample.id , car_id: Car.all.sample.id )
+  Booking.create( start_date: '02/01/2022' , end_date: '09/01/2022', user_id: User.all.sample.id , car_id: Car.all.sample.id )
+  Booking.create( start_date: '01/01/2022' , end_date: '09/01/2022', user_id: User.all.sample.id , car_id: Car.all.sample.id )
