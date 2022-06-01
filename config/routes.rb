d4ac0ae6e435bdd918d6ac3e72634df84896e314
Chayan Agarwal <chayan.agarwal@yahoo.com>
2022-06-01 13:29:34 +0100

    Added create/delete cars and fixed some UI issues.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :cars, only: [:index, :new, :create, :show] do
+  resources :cars, only: [:index, :new, :create, :destroy, :show] do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index]
